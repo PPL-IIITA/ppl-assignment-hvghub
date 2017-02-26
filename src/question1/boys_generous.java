34b7f86b7617fb767f970c2ac3b13ec71f13b473
hvghub <lit2015004@iiita.ac.in>
2017-02-26 23:42:32 +0530

    new file:   boys.csv

---
+++ src/question1/boys_generous.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question1;
+
+/**
+ *
+ * @author Harshit
+ */
+public class boys_generous {
+    String Name;
+    int Atr,Budget,Intelligence,Req_Attract,if_comm;
+    public boys_generous(String Name, int Atr, int Budget, int Intelligence, int Req_Attract) {
+        this.Name = Name;
+        this.Atr = Atr;
+        this.Intelligence = Intelligence;
+        this.Req_Attract = Req_Attract;
+        this.Budget = Budget;
+        this.if_comm = 0;
+    }
+    public String getname(){
+        return Name;
+    }
+    public int getatt(){
+        return Atr;
+    }
+    public int getbudget(){
+        return Budget;
+    }
+    public int getintelligence(){
+        return Intelligence;
+    }
+    public int getreq_attraction(){
+        return Req_Attract;
+    }
+    public void commit(){
+        if_comm = 1;
+    }
+    public int status(){
+        return if_comm;
+    }
+}
